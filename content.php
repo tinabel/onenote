bc8b3ee03005eef899e3780d2a627fce2ce4d278
Tina Bell Vance <tvance@ag.com>
2018-03-22 08:48:22 -0400

    Changing excerpt to plain content, styling for new change

--- content.php
+++ content.php
@@ -1,17 +1,17 @@
     <div>
-        <a href="<?php the_permalink(); ?>" class="post-link">
+        <div onclick="location.href='<?php the_permalink(); ?>'" class="post-link">
             <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <h2 class="post-title"><?php the_title(); ?></h2>            
+                <h2 class="post-title"><?php the_title(); ?></h2>
                 <?php
                  if ( has_post_thumbnail()) {
                     $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large');
                     the_post_thumbnail('thumbnail');
-                the_excerpt();
+                    the_content( $more_link_text = null, $strip_teaser = false );
                 } else {
-                    the_excerpt();
+                    the_content( $more_link_text = ' read more', $strip_teaser = false );
                 } ?>
             </div>
-        </a>
+        </div>
         <p class="tags">
             <?php the_tags();?>
         </p>
