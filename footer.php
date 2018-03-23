77e3e100d2ff728092c2cb8ca83ce5b1dd2d1399
Tina Bell Vance <tvance@ag.com>
2018-03-22 20:38:32 -0400

    adding copyright

--- footer.php
+++ footer.php
@@ -2,6 +2,9 @@
     <p>
         <a href="#">Back to top</a>
     </p>
+    <p>
+        One Note WordPress theme copyright 2018, Tina Bell Vance
+    </p>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <?php wp_footer(); ?>
